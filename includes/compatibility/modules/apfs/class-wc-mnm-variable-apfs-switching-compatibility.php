0682c614267c6aaeffb5f3a3f5e0b37b4d0adef4
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2022-12-19 21:41:55 -0700

    Add variation ID to switch URL

--- includes/compatibility/modules/apfs/class-wc-mnm-variable-apfs-switching-compatibility.php
+++ includes/compatibility/modules/apfs/class-wc-mnm-variable-apfs-switching-compatibility.php
@@ -33,8 +33,12 @@ if ( ! class_exists( 'WC_MNM_Variable_APFS_Switching_Compatibility' ) ) :
 
 			// Add extra 'Allow Switching' options. See 'WCS_ATT_Admin::allow_switching_options'.
 			add_filter( 'woocommerce_subscriptions_allow_switching_options', array( __CLASS__, 'add_container_switching_options' ), 11 );
+			
 			// Add variations to switch link.
 			add_filter( 'wc_mnm_get_posted_container_form_data', array( __CLASS__, 'get_posted_container_form_data' ), 10, 3 );
+
+			// Add current variation ID to switch link.
+			add_filter( 'woocommerce_subscriptions_switch_url', array( __CLASS__, 'container_type_switch_configuration_url' ), 10, 4 );
 
 		}
 
@@ -66,7 +70,7 @@ if ( ! class_exists( 'WC_MNM_Variable_APFS_Switching_Compatibility' ) ) :
 		/**
 		 * Add attributes to variation switch link.
 		 *
-		 * @param  array       $form_data The params that will be used to bbuild siwtch link
+		 * @param  array       $form_data The params that will be used to build switch link
 		 * @param  array       $configuration The container configuration
 		 * @param  WC_Product  $product The container product
 		 * @return boolean
@@ -84,6 +88,35 @@ if ( ! class_exists( 'WC_MNM_Variable_APFS_Switching_Compatibility' ) ) :
 			}
 		
 			return $form_data;
+		}
+
+	
+		/**
+		 * Add variation ID to switch link.
+		 *
+		 * @param  string           $url
+		 * @param  int              $item_id
+		 * @param  WC_Order_Item    $item
+		 * @param  WC_Subscription  $subscription
+		 * @return string
+		 */
+		public static function container_type_switch_configuration_url( $url, $item_id, $item, $subscription ) {
+
+			if ( wc_mnm_is_container_order_item( $item, $subscription ) ) {
+
+				if ( $configuration = WC_Mix_and_Match_Order::get_current_container_configuration( $item, $subscription ) ) {
+
+					$variation_id = $item->get_variation_id();
+
+					if ( $variation_id ) {
+						$url = add_query_arg( 'variation_id', $variation_id, $url );
+					}
+
+				}
+
+			}
+
+			return $url;
 		}
 
 
