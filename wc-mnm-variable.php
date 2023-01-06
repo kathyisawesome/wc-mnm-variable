1b8da69d11dbcdcb8b8f07434feb790ffacc5e0d
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-01-06 16:59:43 -0700

    version bump 1.0.0-rc.3

--- wc-mnm-variable.php
+++ wc-mnm-variable.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce Mix and Match - Variable Mix and Match
  * Plugin URI: 
  * Description: Variable mix and match product type
- * Version: 1.0.0-rc.2
+ * Version: 1.0.0-rc.3
  * Author: Kathy Darling
  * Author URI: http://kathyisawesome.com/
  * Text Domain: wc-mnm-variable
