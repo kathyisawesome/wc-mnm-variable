2cd2e77affe345101d515f2a72a014608a4ca839
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-01-24 13:12:14 -0700

    Relocate APFS fields to before MNM options. Closes #23.

--- includes/compatibility/modules/apfs/class-wc-mnm-variable-apfs-compatibility.php
+++ includes/compatibility/modules/apfs/class-wc-mnm-variable-apfs-compatibility.php
@@ -27,9 +27,6 @@ if ( ! class_exists( 'WC_MNM_Variable_APFS_Compatibility' ) ) :
 			// Register mix-and-match-variation as supported type.
 			add_filter( 'wcsatt_supported_product_types', [ __CLASS__, 'wcsatt_supported_product_types' ] );
 
-			// Change scheme display location.
-			add_filter( 'wcsatt_modify_variation_data_price_html', [ __CLASS__, 'wcsatt_modify_variation_data_price_html' ], 10, 2 );
-
 		}
 		
 		/**
@@ -40,18 +37,6 @@ if ( ! class_exists( 'WC_MNM_Variable_APFS_Compatibility' ) ) :
 		 */
 		public static function wcsatt_supported_product_types( $types ) {
 			return array_merge( $types, [ 'variable-mix-and-match', 'mix-and-match-variation' ] );
-		}
-
-
-		/**
-		 * Force display of schemes to before_add_to_cart_button
-		 *
-		 * @param bool $modify
-		 * @param WC_Product $product
-		 * @return bool
-		 */
-		public static function wcsatt_modify_variation_data_price_html( $modify, $product ) {
-			return $product && $product->is_type( [ 'variable-mix-and-match', 'mix-and-match-variation' ] ) ? false : $modify;
 		}
 
 	} // End class: do not remove or there will be no more guacamole for you.
