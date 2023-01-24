83fa4ae25d44044791e21e24457127eb2c9aac27
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-01-24 13:30:49 -0700

    move templates to template_redirect to match MNM

--- wc-mnm-variable.php
+++ wc-mnm-variable.php
@@ -73,7 +73,7 @@ class WC_MNM_Variable {
 		$this->includes();
 
 		// Load template actions/functions later.
-		add_action( 'after_setup_theme', [ $this, 'template_includes' ], 20 );
+		add_action( 'template_redirect', [ $this, 'template_includes' ], 20 );
 
 		// Include admin class to handle all back-end functions.
 		if ( is_admin() ) {
