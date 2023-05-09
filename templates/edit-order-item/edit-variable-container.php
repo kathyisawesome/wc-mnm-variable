5df93094d64460d70ef8a6ec563e11a4ac7b342c
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-05-09 17:30:34 -0400

    fix template name in docblock

--- templates/edit-order-item/edit-variable-container.php
+++ templates/edit-order-item/edit-variable-container.php
@@ -2,7 +2,7 @@
 /**
  * Mix and Match Product Edit Variable Container
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product/edit-order-item/edit-variable-mix-and-match.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/edit-order-item/edit-variable-container.php.
  *
  * HOWEVER, on occasion WooCommerce Mix and Match will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
