89a4e3a95a2e31584c203975b187840384d1f6ba
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-09-05 15:49:24 -0400

    delete unused context files

--- src/context/ConfigContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from '@wordpress/element';
-
-export const ConfigContext = createContext([]);
--- src/context/ContainerContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from '@wordpress/element';
-
-export const ContainerContext = createContext();
