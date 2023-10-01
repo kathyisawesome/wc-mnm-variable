729e05064aceceac3e8d1a408abd8e2cb1cdf51e
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2023-09-30 23:15:09 -0400

    Begin adding some linting

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+	extends: [ 'plugin:@woocommerce/eslint-plugin/recommended' ],
+	globals: {
+		jQuery: 'readonly',
+		wc_mnm_params: true,
+		WC_MNM_ADD_TO_CART_REACT_PARAMS: true,
+	},
+	rules: {
+		'woocommerce/feature-flag': 'off',
+		'react-hooks/exhaustive-deps': 'error',
+		'react/jsx-fragments': [ 'error', 'syntax' ],
+		'@wordpress/no-global-active-element': 'warn',
+		'@wordpress/i18n-text-domain': [
+			'error',
+			{
+				allowedTextDomain: [ 'woo-gutenberg-products-block' ],
+			},
+		],
+		'@typescript-eslint/no-restricted-imports': [
+			'error',
+			{
+				paths: [
+					{
+						name: 'react',
+						message:
+							'Please use React API through `@wordpress/element` instead.',
+						allowTypeImports: true,
+					},
+				],
+			},
+		],
+		camelcase: [
+			'error',
+			{
+				properties: 'never',
+				ignoreGlobals: true,
+				// allow: [ 'wc_mnm_params', 'WC_MNM_ADD_TO_CART_REACT_PARAMS' ]
+			},
+		],
+		'react/react-in-jsx-scope': 'off',
+        // Turned off because conflicts with the ones above and does not support aliases
+        'node/no-missing-require': 'off',
+        'node/no-extraneous-import':'off',
+	},
+
+	settings: {
+		'import/resolver': {
+            "webpack": {
+                "config": "webpack.config.js"
+              }
+        }
+	},
+};
