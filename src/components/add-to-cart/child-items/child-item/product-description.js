ebb33ad5781c4bdcdca0c08c112910fdf7bd8307
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-02-09 11:27:15 -0500

    wrap short description in p tag

--- src/components/add-to-cart/child-items/child-item/product-description.js
+++ src/components/add-to-cart/child-items/child-item/product-description.js
@@ -3,7 +3,7 @@ import { Interweave } from 'interweave';
 const ProductDescription = ( { shortDescription } ) => {
 	return (
 		<div className="wc-mnm-block-child-item__product-description">
-			<Interweave content={ shortDescription } />
+			<Interweave tagName="p" content={ shortDescription } />
 		</div>
 	);
 };
