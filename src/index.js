117a083b606441eefbaca790645352c268e3113e
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-02-29 14:37:04 -0500

    simplify root rendering. we don't need to wait for an event from core MNM any more (nor do we need to send core an event... we're independent)

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-import { createRoot, render, createPortal } from '@wordpress/element';
+import { createRoot, render } from '@wordpress/element';
 import MixAndMatch from './mix-and-match';
 
-// Attach the event listener to the desired custom event
-document.addEventListener( 'wc-mnm-initialize.variable-mix-and-match', function(e) {
+// Attach the event listener
+const targets = document.querySelectorAll( '.wc-mix-and-match-root' );
 
-	const targets = e.target.querySelectorAll( '.wc-mix-and-match-root' );
+targets.forEach( function ( target ) {
 
-	targets.forEach( function ( target ) {
-
-		if ( createRoot ) {
-			createRoot( target ).render( <MixAndMatch target={ target } /> );
-		} else {
-			render( <MixAndMatch />, target );
-		}
-	} );
-
-});
-
-const initMNM = new CustomEvent( 'wc-mnm-initialize.variable-mix-and-match' );
-
-// Dispatch event on load.
-document.dispatchEvent(initMNM);
+	if ( createRoot ) {
+		createRoot( target ).render( <MixAndMatch target={ target } /> );
+	} else {
+		render( <MixAndMatch />, target );
+	}
+} );
